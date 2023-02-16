4f7cc1eb3cf03097a8db6ceea9aaa71a6263277a
HassanShehab <hassanshehab746@gmail.com>
2023-02-16 15:31:55 +0200

    scound version

--- js/main.js
+++ js/main.js
@@ -3,8 +3,10 @@ let inBtn = document.getElementById('inBtn');
 let outBtn = document.getElementById('darkBtn');
 let widthDiv = document.getElementById('widthDiv');
 let heightDiv = document.getElementById('heightDiv');
+let textDiv = document.getElementById('textDiv');
 let color = document.getElementById('colors');
-let btn = document.getElementById('conBtn');
+let textColor = document.getElementById('textColors');
+let confBtn = document.getElementById('conBtn');
 let div = document.getElementById('target');
 let defult = document.getElementById('defult');
 let curve = document.getElementById('curve');
@@ -13,6 +15,8 @@ let curveValue = document.getElementById('curveValue');
 let borderValue =document.getElementById('borderValue');
 let container = document.getElementById('contain');
 let textInput = document.getElementsByClassName('textInput');
+let borderSec = document.getElementById('borderSec');
+let borderColor = document.getElementById('borderColors');
 
 let modes = ()=>{
     if(body.classList.contains('lightMode')){
@@ -27,6 +31,9 @@ let modes = ()=>{
             textInput[i].style.color = '#121212';
             textInput[i].classList.add('change');
         }
+        if(curve.checked == true && border.checked == true){
+            confBtn.style.borderColor = '#eee';
+        }
     }else if(body.classList.contains('darkMode')){
         body.classList.remove('darkMode');
         body.classList.add('lightMode');
@@ -34,6 +41,7 @@ let modes = ()=>{
         inBtn.style.background = '#eee';
         inBtn.style.left = '2px';
         outBtn.style.background = '#121212';
+        confBtn.style.borderColor = 'black';
         for(let i = 0 ; i < textInput.length ; i++){
             textInput[i].style.background = '#121212';
             textInput[i].style.color = '#eee';
@@ -46,22 +54,26 @@ outBtn.addEventListener('click', modes);
  curve.addEventListener('click' , function(){
     if(curve.checked == true){
         curveValue.classList.remove('hide');
+    }else{
+        curveValue.classList.add('hide');
     }
  });
 
  border.addEventListener('click' , function(){
     if(border.checked == true){
-        borderValue.classList.remove('hide');
+        borderSec.classList.remove('hide');
+    }else{
+        borderSec.classList.add('hide');
     }
  })
 
-btn.addEventListener('click' , function(){
+ confBtn.addEventListener('click' , function(){
     div.style.width = widthDiv.value + "px" ;
     div.style.height = heightDiv.value + "px" ;
     div.style.background = color.value ;
     div.classList.remove('hide');
     if(border.checked == true){
-        div.style.border = borderValue.value + "px solid";
+        div.style.border = borderValue.value + "px solid " + borderColor.value ;
     }else{
         div.style.border = 'none';
     }
@@ -69,8 +81,12 @@ btn.addEventListener('click' , function(){
         div.style.borderRadius = curveValue.value + "px";
     }else{
         div.style.borderRadius = '0px';
-    }
-    if(widthDiv.value !== '' || heightDiv.value !== ''){
-        defult.classList.add('hide');
+    }if(textDiv.value !==''){
+        defult.innerText = textDiv.value ;
+        defult.style.color = textColor.value;    
+    }else if(textDiv.value =='' & widthDiv.value !== ''|| heightDiv.value !== ''){
+        defult.innerText = 'this is a defult text';
+    }else if(textDiv.value =='' & widthDiv.value == ''& heightDiv.value == ''){
+        defult.innerText = 'this is a defult box';
     }
 })
